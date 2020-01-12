bd37ff7ff499e2decfeccd8389d8012b6ed2cc66
GabMic <ruinsain@gmail.com>
2020-01-12 13:24:34 +0200

    wip

--- resources/views/partials/overview.blade.php
+++ resources/views/partials/overview.blade.php
@@ -22,24 +22,6 @@
                 </div>
             </div>
         </div>
-        <div class="tile is-parent">
-            <div class="card tile is-child">
-                <div class="card-content">
-                    <div class="level is-mobile">
-                        <div class="level-item">
-                            <div class="is-widget-label">
-                                <h5 class="subtitle is-5 is-spaced">@lang('general.open-notes')</h5>
-                                <h1 class="title is-1-mobile">{{$tasks ? count($tasks) : 0}}</h1>
-                            </div>
-                        </div>
-                        <div class="level-item has-widget-icon">
-                            <div class="is-widget-icon"><span class="icon has-text-info is-large"><i class="mdi mdi-note-multiple-outline mdi-48px"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <div class="tile is-parent">
             <div class="card tile is-child">
@@ -53,6 +35,25 @@
                         </div>
                         <div class="level-item has-widget-icon">
                             <div class="is-widget-icon"><span class="icon has-text-success is-large"><i class="mdi mdi-percent-outline mdi-48px"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="tile is-parent">
+            <div class="card tile is-child">
+                <div class="card-content">
+                    <div class="level is-mobile">
+                        <div class="level-item">
+                            <div class="is-widget-label">
+                                <h5 class="subtitle is-5 is-spaced">@lang('general.open-notes')</h5>
+                                <h1 class="title is-1-mobile">{{$tasks ? count($tasks) : 0}}</h1>
+                            </div>
+                        </div>
+                        <div class="level-item has-widget-icon">
+                            <div class="is-widget-icon"><span class="icon has-text-info is-large"><i class="mdi mdi-note-multiple-outline mdi-48px"></i></span>
                             </div>
                         </div>
                     </div>
