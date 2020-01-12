fc96bdf72d2957f00457abd6cc27a7b5968d6531
GabMic <ruinsain@gmail.com>
2020-01-12 12:52:27 +0200

    wip

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -1,18 +1,11 @@
 @extends('layouts.app')
 
 @section('title', 'התקציב שלי')
-@section('css')
-    <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet">
-    <style>
-        body{
-            font-family: 'Varela Round', sans-serif;
-        }
-    </style>
-@endsection
+
 @section('content')
     <div class="columns m-1">
         <div class="column is-4">
-            <h5>קביעת התקציב עבור חודש {{\Carbon\Carbon::now()->month}}</h5>
+            <h5 class="subtitle is-5">קביעת התקציב עבור חודש <b>{{Carbon\Carbon::now()->monthName}}</b></h5>
             <hr>
             <form action="{{route('budget.store')}}" method="post">
                 @csrf
@@ -26,11 +19,16 @@
         </div>
         <div class="column is-4">
             <div class="box">
-                <h6> התקציב שקבעתי עבור חודש <strong>{{\Carbon\Carbon::now()->month}}</strong>:  <strong>{{number_format($currentMonthBudget)}}</strong> ש"ח.</h6>
+                <h6> התקציב שקבעתי עבור חודש <strong>{{Carbon\Carbon::now()->monthName}}</strong>:  <strong>{{number_format($currentMonthBudget)}}</strong> ש"ח.</h6>
                 <hr>
                 סך ההוצאות שלי החודש עד כה: <strong>{{number_format($totalExpensesThisMonth)}}</strong> ש"ח.
                 <hr>
-                תקציב לאחר הוצאות: <strong>{{number_format($currentMonthBudget - $totalExpensesThisMonth)}}</strong> ש"ח.
+                תקציב שנותר לאחר ההוצאות:
+                @if($currentMonthBudget - $totalExpensesThisMonth < 0 )
+                    <strong style="color: red">{{number_format($currentMonthBudget - $totalExpensesThisMonth)}}</strong>
+                @elseif($currentMonthBudget - $totalExpensesThisMonth >= 0)
+                    <strong style="color: green">{{number_format($currentMonthBudget - $totalExpensesThisMonth)}}</strong>
+                @endif ש"ח.
             </div>
         </div>
         <div class="column is-4">
